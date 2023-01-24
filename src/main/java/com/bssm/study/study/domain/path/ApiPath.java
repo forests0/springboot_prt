4f137bcb863901921633a178bea0b4472db983ed
Chell2358 <forestser00@gmail.com>
2023-01-24 09:28:51 +0900

    apipath

---
+++ src/main/java/com/bssm/study/study/domain/path/ApiPath.java
@@ -0,0 +1,11 @@
+package com.bssm.study.study.domain.path;
+
+public class ApiPath {
+
+    public static final String MEMBER = "/api/member";
+    public static final String MEMBER_ID = "/api/member/{member-id}";
+
+    public static final String MEMBER_EMAIL = "/api/member/email/{member-email}";
+
+    public static final String PORTFOLIO = "";
+}
